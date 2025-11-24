0dcf1f01af06f90e770ce42b6f8ef66cccbf1bba
Ahmet Kerem Sezgin <asezgin@k2m02s01.42kocaeli.com.tr>
2025-11-24 10:24:50 +0300

    Cpp02 yorum satrıları kaldırıdldı

--- CPP Module 02/ex02/Fixed.hpp
+++ CPP Module 02/ex02/Fixed.hpp
@@ -1,24 +1,6 @@
 #pragma once
 #include <iostream>
 
-
-/* 
-• The 6 comparison operators: >, <, >=, <=, ==, and !=.
-• The 4 arithmetic operators: +, -, *, and /.
-• The 4 increment/decrement (pre-increment and post-increment, pre-decrement and
-post-decrement) operators, which will increase or decrease the fixed-point value by
-the smallest representable ϵ, such that 1 + ϵ > 1.
-Add these four public overloaded member functions to your class:
-• A static member function min that takes two references to fixed-point numbers as
-parameters, and returns a reference to the smallest one.
-• A static member function min that takes two references to constant fixed-point
-numbers as parameters, and returns a reference to the smallest one.
-• A static member function max that takes two references to fixed-point numbers as
-parameters, and returns a reference to the greatest one.
-• A static member function max that takes two references to constant fixed-point
-numbers as parameters, and returns a reference to the greatest one.
-
-*/
 class Fixed {
     private:
         int number;
