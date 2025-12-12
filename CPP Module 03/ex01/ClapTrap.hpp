fdbbb67d237100774ac79091fafd8311258f466f
Ahmet Kerem Sezgin <asezgin@k2m01s01.42kocaeli.com.tr>
2025-12-12 20:23:46 +0300

    ex01 bitti

---
+++ CPP Module 03/ex01/ClapTrap.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+class ClapTrap
+{
+    protected: 
+    std::string name;
+    int hit_point;
+    int energy_point;
+    int attack_damage;
+    public:
+    ClapTrap(std::string name);
+    ClapTrap(const ClapTrap &other);
+    void attack(const std::string &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    ~ClapTrap();
+};
