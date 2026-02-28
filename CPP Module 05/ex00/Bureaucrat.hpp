db79ba9f8c0446a07d34afcb7d2be0356125adf2
Ahmet Kerem Sezgin <asezgin@k1m18s02.42kocaeli.com.tr>
2026-02-28 16:34:00 +0300

    ex00 Push

--- CPP Module 05/ex00/Bureaucrat.hpp
+++ CPP Module 05/ex00/Bureaucrat.hpp
@@ -1,29 +1,39 @@
-# define BUREAUCRAT_HPP
-# ifdef BUREAUCRAT_HPP
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
 
 #include <iostream>
 
-class Bureaucrat
-{
-    private:
-    const std::string name; 
+class Bureaucrat {
+private:
+    const std::string _name;
     int _grade;
+public:
+    Bureaucrat(const std::string name, int grade);
+    Bureaucrat(const Bureaucrat &src);
+    Bureaucrat &operator=(const Bureaucrat &rhs);
+    ~Bureaucrat();
 
+    std::string getName() const;
+    int getGrade() const;
+
+    void incrementGrade();
+    void decrementGrade();
+
+    class GradeTooHighException : public std::exception {
     public:
-    Bureaucrat() {};
-    class GradeTooLowException : public std::exception {
-        public:
-            virtual const char* what() const throw() {
-                return "Grade too low";
-            }
+        virtual const char* what() const throw() {
+            return "Grade is too high! (Must be >= 1)";
+        }
     };
 
+    class GradeTooLowException : public std::exception {
+    public:
+        virtual const char* what() const throw() {
+            return "Grade is too low! (Must be <= 150)";
+        }
+    };
 };
 
-#endif
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj);
 
-try {
-    throw 1;
-} catch (int a) {
-    std::cout << a << std::endl;
-}
+#endif
--- CPP Module 05/ex00/main.cpp
+++ CPP Module 05/ex00/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include "Bureaucrat.hpp"
 
-int main()
-{
-    
+int main() {
+    try {
+        Bureaucrat a("Ahmet", 2);
+        std::cout << a << std::endl;
+        a.incrementGrade();
+        std::cout << "After increment: " << a << std::endl;
+        a.incrementGrade();
+    }
+    catch (const std::exception &e) {
+        std::cerr << "Test 1 Error: " << e.what() << std::endl;
+    }
+
+    std::cout << "---" << std::endl;
+
+    try {
+        Bureaucrat b("Bürokrat X", 151);
+    }
+    catch (const std::exception &e) {
+        std::cerr << "Test 2 Error: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
