fccd3b76ec98629e8fd48287c9842b57c58c0e28
Ahmet Kerem Sezgin <asezgin@k2m09s02.42kocaeli.com.tr>
2026-03-07 10:31:13 +0300

    Final Push

---
+++ CPP Module 05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,24 @@
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(std::string target) 
+    : AForm("Robotomy Request", 25, 5), _target(target) {}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &src) 
+    : AForm(src), _target(src._target) {}
+
+PresidentialPardonForm::~PresidentialPardonForm() {}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &rhs) {
+    (void)rhs;
+    return *this;
+}
+
+void PresidentialPardonForm::execute(Bureaucrat const & executor) const {
+    if (!this->getIsSigned())
+        throw std::runtime_error("Error: Form is not signed yet!");
+
+    if (executor.getGrade() > this->getExecGrade())
+        throw AForm::GradeTooLowException();
+
+        std::cout << _target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
