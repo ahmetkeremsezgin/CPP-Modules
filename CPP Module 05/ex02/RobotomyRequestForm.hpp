6987f05e08699121f85df24df72da01902b9368d
Ahmet Kerem Sezgin <asezgin@k2m09s02.42kocaeli.com.tr>
2026-03-07 10:21:50 +0300

    Roboty push

---
+++ CPP Module 05/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,22 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+
+
+class RobotomyRequestForm : public AForm {
+private:
+    const std::string _target;
+    RobotomyRequestForm(); 
+
+public:
+    RobotomyRequestForm(const std::string target);
+    RobotomyRequestForm(const RobotomyRequestForm &src);
+    virtual ~RobotomyRequestForm(); 
+
+    RobotomyRequestForm &operator=(const RobotomyRequestForm &rhs);
+
+    void execute(Bureaucrat const & executor) const;
+};
+
+#endif
--- CPP Module 05/ex02/main.cpp
+++ CPP Module 05/ex02/main.cpp
@@ -1,26 +1,24 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include <ctime>
+#include <cstdlib>
 
 int main() {
+    std::srand(std::time(NULL));
     try {
-        // 1. Bir bürokrat oluşturuyoruz (İsim: Bob, Derece: 1)
         Bureaucrat bob("Bob", 1);
 
-        // 2. Formu oluşturuyoruz ve "target" olarak "home" veriyoruz.
-        // Bu işlem sonucunda "home_shrubbery" dosyası oluşturulacak.
-        ShrubberyCreationForm form1("home");
+        RobotomyRequestForm form1("home");
 
-        std::cout << form1 << std::endl; // Form bilgilerini yazdır
+        std::cout << form1 << std::endl;
 
-        // 3. Bürokrat formu imzalamaya çalışıyor
         bob.signForm(form1);
 
-        // 4. Bürokrat formu çalıştırmaya (execute) çalışıyor
         bob.executeForm(form1);
 
     } catch (std::exception &e) {
         std::cerr << "Hata oluştu: " << e.what() << std::endl;
     }
-
     return 0;
 }
